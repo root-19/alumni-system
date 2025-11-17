8b8dbd0d32bc5e731a2354be8b3b02367af257b9
Rens  Acuña <127023454+root-19@users.noreply.github.com>
2025-11-16 20:51:45 -0800

    push

--- database/migrations/2025_08_31_032613_rename_post_id_to_alumni_post_id_in_comments_table.php
+++ database/migrations/2025_08_31_032613_rename_post_id_to_alumni_post_id_in_comments_table.php
@@ -1,28 +1,25 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('comments', function (Blueprint $table) {
-            $table->renameColumn('post_id', 'alumni_post_id');
-        });
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up() {
+        if (Schema::hasColumn('comments', 'post_id')) {
+            Schema::table('comments', function (Blueprint $table) {
+                $table->renameColumn('post_id', 'alumni_post_id');
+            });
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('comments', function (Blueprint $table) {
-            $table->renameColumn('alumni_post_id', 'post_id');
-        });
+    public function down() {
+        if (Schema::hasColumn('comments', 'alumni_post_id')) {
+            Schema::table('comments', function (Blueprint $table) {
+                $table->renameColumn('alumni_post_id', 'post_id');
+            });
+        }
     }
 };
+
