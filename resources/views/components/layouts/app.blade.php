c37169207d2964834623c5c275324abfef7ce0b3
Rens  Acuña <127023454+root-19@users.noreply.github.com>
2026-04-02 18:51:12 -0700

    push

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -3,7 +3,6 @@
     <head>
         @include('partials.head')
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
         <style>[x-cloak] { display: none !important; }</style>
         @stack('scripts')
     </head>
